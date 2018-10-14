75c7fac4724ea56d69b7c1510471756ea56d8e0c
suite <amiysd@163.com>
2018-10-14 22:32:42 +0800

    init

--- http/routes/amiba.go
+++ http/routes/amiba.go
@@ -4,25 +4,43 @@ import (
 	"github.com/absuite/suite-runtime/http/controllers/amiba"
 	"github.com/absuite/suite-runtime/repositories"
 	"github.com/absuite/suite-runtime/services/amiba"
+	"github.com/absuite/suite-runtime/services/cbo"
 	"github.com/ggoop/goutils/glog"
-
+	"github.com/kataras/iris/hero"
 	"github.com/kataras/iris/mvc"
 )
 
 func registerAmiba(c *Context) {
-	//model route
 	repo := repositories.NewModelRepo(c.Orm)
-	modelSv := amibaServices.NewModelSv(repo)
+	//组织服务
+	hero.Register(cboServices.NewOrgSv(repo))
+	//期间服务
+	hero.Register(cboServices.NewPeriodSv(repo))
+	//部门服务
+	hero.Register(cboServices.NewDeptSv(repo))
+	//物料服务
+	hero.Register(cboServices.NewItemSv(repo))
+	//存储地点服务
+	hero.Register(cboServices.NewWhSv(repo))
+	//项目服务
+	hero.Register(cboServices.NewWhSv(repo))
+	//单据类型服务
+	hero.Register(cboServices.NewDocTypeSv(repo))
+
+	//阿米巴模型服务
+	hero.Register(amibaServices.NewModelSv(repo))
+	//价表服务
+	hero.Register(amibaServices.NewPricelSv(repo))
+
+	//model route
+
 	modeling := mvc.New(c.App.Party("/api/amiba/models"))
-	modeling.Register(modelSv)
 	modeling.Handle(new(amibaControllers.ModelController))
 	glog.Printf("注册 %s 路由成功", "/api/amiba/models")
 
 	//price route
-	priceSv := amibaServices.NewPricelSv(repo)
-	priceSv.CacheAll() //缓存价表数据
+
 	price := mvc.New(c.App.Party("/api/amiba/prices"))
-	price.Register(priceSv)
 	price.Handle(new(amibaControllers.PriceController))
 	glog.Printf("注册 %s 路由成功", "/api/amiba/prices")
 }
